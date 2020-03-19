7ee8d8479e4729ee467645ce8b010942d68c9243
Volpanic <volpanicstudios@gmail.com>
2020-03-20 10:06:26 +1100

    Completed Vector3 and Vector4, Still subject to testing.

--- MathClasses/Vector3.cs
+++ MathClasses/Vector3.cs
@@ -17,6 +17,8 @@ namespace MathClasses
             z = _z;
         }
 
+        #region Operators
+
         //Addition
         public static Vector3 operator +(Vector3 a, Vector3 b)
         {
@@ -41,11 +43,27 @@ namespace MathClasses
             return new Vector3(a.x * scale, a.y * scale, a.z * scale);
         }
 
+        //Division F V
+        public static Vector3 operator /(float divis, Vector3 a)
+        {
+            return new Vector3(a.x / divis, a.y / divis, a.z / divis);
+        }
+
+        //Division V F
+        public static Vector3 operator /(Vector3 a, float divis)
+        {
+            return new Vector3(a.x / divis, a.y / divis, a.z / divis);
+        }
+
+        #endregion// End Operators
+
+        //Dot product
         public float Dot(Vector3 comp)
         {
             return ((x * comp.x) + (y * comp.y) + (z * comp.z));
         }
 
+        //Corss Product
         public Vector3 Cross(Vector3 comp)
         {
             float xx = (y * comp.z) - (z * comp.y);
@@ -53,6 +71,18 @@ namespace MathClasses
             float zz = (x * comp.y) - (y * comp.x);
 
             return new Vector3(xx,yy,zz);
+        }
+
+        //Magnitude
+        public float Magnitude()
+        {
+            return (float)(Math.Sqrt((x * x) + (y * y) + (z * z)));
+        }
+
+        //Normalize
+        public Vector3 Normalize()
+        {
+            return this / Magnitude();
         }
 
     }
--- MathClasses/Vector4.cs
+++ MathClasses/Vector4.cs
@@ -10,12 +10,71 @@ namespace MathClasses
     {
         public float x, y, z, w;
 
-        public Vector4()
+        public Vector4(float _x, float _y, float _z, float _w)
         {
-            x = 0;
-            y = 0;
-            z = 0;
-            w = 0;
+            x = _x;
+            y = _y;
+            z = _z;
+            w = _w;
+        }
+
+        #region Operators
+
+        //Addition
+        public static Vector4 operator +(Vector4 a, Vector4 b)
+        {
+            return new Vector4(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
+        }
+
+        //Subtraction
+        public static Vector4 operator -(Vector4 a, Vector4 b)
+        {
+            return new Vector4(a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w);
+        }
+
+        //Multiplication V F
+        public static Vector4 operator *(Vector4 a, float scale)
+        {
+            return new Vector4(a.x * scale, a.y * scale, a.z * scale, a.w * scale);
+        }
+
+        //Multiplication F V
+        public static Vector4 operator *(float scale, Vector4 a)
+        {
+            return new Vector4(a.x * scale, a.y * scale, a.z * scale, a.w * scale);
+        }
+
+        //Division F V
+        public static Vector4 operator /(float divis, Vector4 a)
+        {
+            return new Vector4(a.x / divis, a.y / divis, a.z / divis, a.w / divis);
+        }
+
+        //Division V F
+        public static Vector4 operator /(Vector4 a, float divis)
+        {
+            return new Vector4(a.x / divis, a.y / divis, a.z / divis, a.w / divis);
+        }
+
+        #endregion//End Operators
+        
+        //Dot product
+        public float Dot(Vector4 comp)
+        {
+            return ((x * comp.x) + (y * comp.y) + (z * comp.z) + (w *comp.w));
+        }
+
+        //Cross
+        public Vector4 Cross(Vector4 comp)
+        {
+            Vector3 temp = new Vector3(comp.x, comp.y, comp.z).Cross(new Vector3(comp.x,comp.y,comp.z));
+            return new Vector4(temp.x,temp.y,temp.z,0);
+        }
+
+        //Magnitude
+        public float Magnitude()
+        {
+            return (float)(Math.Sqrt((x * x) + (y * y) + (z * z) + (w * w)));
         }
 
     }
