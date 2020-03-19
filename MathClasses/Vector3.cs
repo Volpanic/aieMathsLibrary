26e2cfe63e1b34edd8766e4cc73413d53f0289f3
Volpanic <volpanicstudios@gmail.com>
2020-03-19 16:37:15 +1100

    Started work on the Vector3 Class, implemeted Addition, Subtraction, Multiplication (F * V && V * F), Dot, Cross.

--- MathClasses/Vector3.cs
+++ MathClasses/Vector3.cs
@@ -10,11 +10,49 @@ namespace MathClasses
     {
         public float x, y, z;
 
-        public Vector3()
+        public Vector3(float _x, float _y, float _z)
         {
-            x = 0;
-            y = 0;
-            z = 0;
+            x = _x;
+            y = _y;
+            z = _z;
+        }
+
+        //Addition
+        public static Vector3 operator +(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+
+        //Subtraction
+        public static Vector3 operator -(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
+        }
+
+        //Multiplication V F
+        public static Vector3 operator *(Vector3 a, float scale)
+        {
+            return new Vector3(a.x * scale, a.y * scale, a.z * scale);
+        }
+
+        //Multiplication F V
+        public static Vector3 operator *(float scale,Vector3 a)
+        {
+            return new Vector3(a.x * scale, a.y * scale, a.z * scale);
+        }
+
+        public float Dot(Vector3 comp)
+        {
+            return ((x * comp.x) + (y * comp.y) + (z * comp.z));
+        }
+
+        public Vector3 Cross(Vector3 comp)
+        {
+            float xx = (y * comp.z) - (z * comp.y);
+            float yy = (z * comp.x) - (x *comp.z);
+            float zz = (x * comp.y) - (y * comp.x);
+
+            return new Vector3(xx,yy,zz);
         }
 
     }
